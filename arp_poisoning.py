cc9b3fefae53b46db4afb74c624ffa6b7ec6157e
Shir01001 <pr0xy-server@proton.me>
2024-12-08 16:33:55 +0100

    Adding basic arguments and functions for basic structure

---
+++ arp_poisoning.py
@@ -0,0 +1,6 @@
+
+def arp_poisoning_loop():
+    pass
+
+if __name__ == "__main__":
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import argparse
+import pyshark
+
+from arp_poisoning import arp_poisoning_loop
+
+
+def sniffer_loop():
+    pass
+
+
+def start_sniffer(interface_to_capture_packets, mac_address="", ip_address="", verbosity=0):
+    arp_poisoning_loop()
+    sniffer_loop()
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--interface')
+    parser.add_argument('-m', '--mac_address')
+    parser.add_argument('-a', "--address")
+    parser.add_argument('-v', '--verbosity')
+    args = parser.parse_args()
+
+    start_sniffer(args.interface, args.mac_address, args.address, args.verbosity)
+
     print("something")
