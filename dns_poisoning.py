3ee760c1c665f4aa6d92a78440852bdd1f7223ba
Shir01001 <pr0xy-server@proton.me>
2024-12-18 17:07:24 +0100

    Added redirect in http_server.py, Added basic routing configuration functions to dns_poisoning.py, changed README.md

--- dns_poisoning.py
+++ dns_poisoning.py
@@ -1,2 +1,10 @@
-def dns_poisoning():
+import os
+
+def start_dns_poisoning():
     pass
+
+def add_routing_rule():
+    os.system("iptables -I FORWARD -j NFQUEUE --queue-num 0")
+
+def remove_routing_rule():
+    os.system("iptables -D FORWARD -j NFQUEUE --queue-num 0")
