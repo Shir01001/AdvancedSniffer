b69dee6ea8f3bcf4656e4a388f9b453a18d2b6f4
Shir01001 <pr0xy-server@proton.me>
2024-12-18 14:53:27 +0100

    added passing of arguments in main and defaults

--- main.py
+++ main.py
@@ -6,22 +6,23 @@ from arp_poisoning import start_arp_poisoning
 from http_server import http_server_start
 
 
-def initialize_program(interface_pc="wlan0"):
+def initialize_program(interface_pc, mac_address, ip_address, verbosity):
+
     http_server_start()
-    # start_arp_poisoning()
-    start_sniffer(interface_pc, args.mac_address, args.address, args.verbosity)
+    start_arp_poisoning()
+    #start_sniffer(interface_pc, mac_address, ip_address, verbosity)
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--interface')
+    parser.add_argument('-i', '--interface', default="wlan0")
     parser.add_argument('-m', '--mac_address')
     parser.add_argument('-a', "--address")
-    parser.add_argument('-v', '--verbosity')
+    parser.add_argument('-v', '--verbosity', default=0)
     args = parser.parse_args()
 
-    initialize_program(args.interface)
+    initialize_program(args.interface, args.mac_address, args.address, args.verbosity)
     while True:
         command = input("#>")
         match command:
